b27e9c737ad17e0826bb15bb7a076115939f018a
Ashish Mainali <19223717@brookes.com>
2023-01-22 19:22:03 +0000

    - Update product detail view - Update add product detail page

--- products/views.py
+++ products/views.py
@@ -7,9 +7,18 @@ class ProductListView(ListView):
     template_name = 'products/list.html'
     queryset = Product.objects.all()
 
+
 def product_list_view(request):
     querySet = Product.objects.all()
     context = {
         'querySet' : querySet
     }
     return render(request, 'products/list.html', context)
+
+
+def product_detail_view(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'products/detail.html', context)
