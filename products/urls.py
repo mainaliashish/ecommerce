14cbcc59ea30fe4399535458b948b6754f522194
Ashish Mainali <19223717@brookes.com>
2023-01-22 19:21:35 +0000

    - Update product url

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 # URL patterns for products app
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='products'),
+    path('product/<str:pk>/', views.product_detail_view, name="product"),
 ]
