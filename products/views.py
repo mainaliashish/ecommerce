b06ca959a43446405ae20bbe117dda616bf15187
Ashish Mainali <19223717@brookes.com>
2023-01-22 19:13:34 +0000

    - Update views with class based and function based views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
+from django.views.generic import ListView
 
-# Create your views here.
+
+class ProductListView(ListView):
+    template_name = 'products/list.html'
+    queryset = Product.objects.all()
+
+def product_list_view(request):
+    querySet = Product.objects.all()
+    context = {
+        'querySet' : querySet
+    }
+    return render(request, 'products/list.html', context)
