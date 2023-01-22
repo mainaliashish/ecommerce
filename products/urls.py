9916446861911ae8f8260b5ab7e096fe4a062309
Ashish Mainali <19223717@brookes.com>
2023-01-22 21:30:51 +0000

    - Update add featured list url

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 # URL patterns for products app
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='products'),
+    path('featured-products/', views.product_featured_view, name='featured-products'),
     path('product/<str:pk>/', views.product_detail_view, name="product"),
 ]
