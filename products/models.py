b0d73eac237cb6318050df1b0c27e7cca6060bdd
Ashish Mainali <19223717@brookes.com>
2023-01-22 18:36:05 +0000

    - Update add fields for products - Update add migration for products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=250,null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=20, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
