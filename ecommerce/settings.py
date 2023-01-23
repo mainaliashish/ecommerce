2ac30d01d4c48fb180ed18a61d14f5f04702f6c9
Ashish Mainali <19223717@brookes.com>
2023-01-23 13:32:03 +0000

    - Update settings with installed apps

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -21,7 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+    'products',
+    'search',
+    'tags'
 ]
 
 MIDDLEWARE = [
