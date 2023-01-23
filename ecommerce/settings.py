8df920945aede0604779bc86c3eb3a43f24e9973
Ashish Mainali <19223717@brookes.com>
2023-01-23 13:38:48 +0000

    - Update make product title unique

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom apps
     'products',
     'search',
     'tags'
--- products/models.py
+++ products/models.py
@@ -68,7 +68,7 @@ class ProductManager(models.Manager):
 
 # Product model
 class Product(models.Model):
-    title = models.CharField(max_length=250, null=True, blank=True)
+    title = models.CharField(max_length=250, null=True, blank=True, unique=True)
     slug = models.SlugField(blank=True, null=True, unique=True, editable=False)
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(
