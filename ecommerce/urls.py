5418efbed3b2d5a75d61add8aaab543c7bf98f8f
Ashish Mainali <19223717@brookes.com>
2023-01-23 12:20:53 +0000

    - Update main app urls with search app

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
+    path('search/', include('search.urls')),
     path('', include('products.urls') )
 ]
 
