6cea91ff2b6248ae3a83aaa7debbdaf49d505e7b
Ashish Mainali <19223717@brookes.com>
2023-01-24 11:48:50 +0000

    - Update create cart model and migrations - Update add cart model to admin

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, blank=True)
+    total = models.DecimalField(max_digits=7, decimal_places=3, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.id
