c156de50da0ae6236868ffeac8944871ac483402
Ashish Mainali <19223717@brookes.com>
2023-01-24 13:18:28 +0000

    - Update cart manager part one

--- carts/models.py
+++ carts/models.py
@@ -3,6 +3,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 
+class CartManager(models.Manager):
+    def new(self, user=None):
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated:
+                user_obj = user
+        return self.model.objects.create(user=user_obj)
+
 # Create your models here.
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
@@ -10,8 +18,8 @@ class Cart(models.Model):
     total = models.DecimalField(max_digits=7, decimal_places=3, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True,
-                          primary_key=True, editable=False)
+
+    objects = CartManager()
 
     def __str__(self):
-        return self.id
+        return str(self.id)
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Cart
 
 def cart_home(request):
+    cart_id = request.session.get("cart_id", None)
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
+        print("Cart ID exists.")
+        cart_obj = qs.first()
+        if request.user.is_authenticated and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
+    else:
+        cart_obj = Cart.objects.new(user=request.user)
+        request.session['cart_id'] = cart_obj.id
     context = {}
     return render(request, 'carts/home.html', context)
