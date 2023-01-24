4a548ea96881f570e0b65d8741df0ebf3b1ca73a
Ashish Mainali <19223717@brookes.com>
2023-01-24 17:30:37 +0000

    - Update sub total - Update signals to add total amount

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.db.models.signals import pre_save, m2m_changed
 from django.contrib.auth.models import User
 from products.models import Product
 
@@ -8,14 +9,13 @@ class CartManager(models.Manager):
         cart_id = request.session.get("cart_id", None)
         qs = self.get_queryset().filter(id=cart_id)
         if qs.count() == 1:
-            print("Cart ID exists.")
             new_obj = False
             cart_obj = qs.first()
             if request.user.is_authenticated and cart_obj.user is None:
                 cart_obj.user = request.user
                 cart_obj.save()
         else:
-            cart_obj = self.new(user=request.user)
+            cart_obj = Cart.objects.new(user=request.user)
             new_obj = True
             request.session['cart_id'] = cart_obj.id
 
@@ -32,6 +32,7 @@ class CartManager(models.Manager):
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, blank=True)
+    sub_total = models.DecimalField(max_digits=7, decimal_places=3, default=0.00)
     total = models.DecimalField(max_digits=7, decimal_places=3, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
@@ -40,3 +41,25 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+# Register signal to update total
+
+def pre_save_cart_receiver(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        products = instance.products.all()
+        total = 0
+        for item in products:
+            total += item.price
+        if instance.sub_total != total:
+            instance.sub_total = total
+            instance.save()
+
+
+# Connect to pre_save signals
+m2m_changed.connect(pre_save_cart_receiver, sender=Cart.products.through)
+
+
+def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+    instance.total = instance.sub_total
+
+pre_save.connect(pre_save_cart_receiver, sender=Cart)
--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from .models import Cart
 
 def cart_home(request):
-    cart_obj = Cart.objects.new_or_get(request)
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    # products = cart_obj.products.all()
+    # # import pdb;
+    # # pdb.set_trace()
+    # total = 0
+    # for item in products:
+    #     total += item.price
+    # cart_obj.total = total
+    # cart_obj.save()
     context = {}
     return render(request, 'carts/home.html', context)
