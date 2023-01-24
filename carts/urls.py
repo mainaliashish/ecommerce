83a8a7724b670c196c2c5329b0dd75bc56232bfc
Ashish Mainali <19223717@brookes.com>
2023-01-24 18:01:30 +0000

    - Update add cart update

--- carts/urls.py
+++ carts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # URL patterns for products app
 urlpatterns = [
-    path('', views.cart_home, name='cart-home'),
+    path('', views.cart_home, name='home'),
+    path('update/', views.cart_update, name='update'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cart
+from products.models import Product
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    # products = cart_obj.products.all()
-    # # import pdb;
-    # # pdb.set_trace()
-    # total = 0
-    # for item in products:
-    #     total += item.price
-    # cart_obj.total = total
-    # cart_obj.save()
     context = {}
     return render(request, 'carts/home.html', context)
+
+def cart_update(request):
+    product = Product.objects.all().first()
+    product_id = product.id
+    product_obj = Product.objects.get(id=product_id)
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    cart_obj.products.add(product_obj)
+    return redirect('cart:home')
+
